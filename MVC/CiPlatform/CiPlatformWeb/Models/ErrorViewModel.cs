b63290af97074b01f7225a1ec0c842c46afe9a5e
Hemal Patel <pci94@server7.com>
2023-02-10 18:52:30 +0530

    mvc login page

---
+++ MVC/CiPlatform/CiPlatformWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CiPlatformWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
