01e996736dc585d4455d08e0e1bb073b096b023e
Hemal Patel <hemal04121@gmail.com>
2023-02-17 18:35:10 +0530

    home page grid view and list view in html

--- MVC/CiPlatform/CiPlatformWeb/Controllers/HomeController.cs
+++ MVC/CiPlatform/CiPlatformWeb/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace CiPlatformWeb.Controllers
             return View();
         }
 
+        public IActionResult HomePage ()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error ()
         {
