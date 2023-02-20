406375e1c0379015a894ee6da80d922813a2c4d1
Hemal Patel <hemal04121@gmail.com>
2023-02-20 18:45:16 +0530

    responsive pages in html

---
+++ MVC/CiPlatform/CiPlatformWeb/Controllers/MissionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CiPlatformWeb.Controllers
+{
+    public class MissionController : Controller
+    {
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+        public IActionResult ListView ()
+        {
+            return View();
+        }
+    }
+}
